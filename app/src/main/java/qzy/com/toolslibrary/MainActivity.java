23a996f0aad2737ffc01f95b759e1f505a2e8e2e
qzy <43081438@qq.com>
2018-02-01 15:48:33 +0800

    add

--- app/src/main/java/qzy/com/toolslibrary/MainActivity.java
+++ app/src/main/java/qzy/com/toolslibrary/MainActivity.java
@@ -1,13 +1,34 @@
 package qzy.com.toolslibrary;
 
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import qzy.com.utilslib.Person;
+import qzy.com.utilslib.svgText.VectorCompatTextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    VectorCompatTextView homePageIcon;
+    boolean isChecked = true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        homePageIcon = (VectorCompatTextView) findViewById(R.id.homePageIcon);
+        //setSelectorColor(homePageIcon);
+
+        homePageIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isChecked = !isChecked;
+                if(isChecked){
+                    homePageIcon.setTextColor(ContextCompat.getColor(MainActivity.this,R.color.colorf84848));
+                }else{
+                    homePageIcon.setTextColor(ContextCompat.getColor(MainActivity.this,R.color.color333333));
+                }
+            }
+        });
     }
 }
---
+++ utilslib/src/main/java/qzy/com/utilslib/Person.java
@@ -0,0 +1,12 @@
+package qzy.com.utilslib;
+
+/**
+ * 作者：quzongyang
+ * <p>
+ * 创建时间：2018/1/31
+ * <p>
+ * 类描述：
+ */
+
+public class Person {
+}
